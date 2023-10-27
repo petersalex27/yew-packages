d65092c119fb1fa04d59e94705a5cb2ffc9ee9ba
Alex Peters <92709569+petersalex27@users.noreply.github.com>
2023-10-27 17:40:46 -0500

    update NameContext expression

--- expr/let.go
+++ expr/let.go
@@ -18,6 +18,54 @@ type NameContext[T nameable.Nameable] struct {
 	contextualized Expression[T]
 }
 
+// Creates a non-tailed NameContext expression; in Haskell this would be a 
+// "let-in" expression:
+// 		let name = assignment in contextualized
+//
+// NOTE: panics if `assignment` is nil
+func Let[T nameable.Nameable](name Const[T], assignment Expression[T], contextualized Expression[T]) NameContext[T] {
+	if assignment == nil {
+		panic("nil value error: argument passed for parameter `assignemnt` cannot be nil")
+	}
+
+	return NameContext[T]{false, name, assignment, contextualized}
+}
+
+// Creates a tailed NameContext expression; in Haskell this would be a "where"
+// expression:
+// 		contextualized where name = assignment
+//
+// NOTE: panics if `assignment` is nil
+func Where[T nameable.Nameable](contextualized Expression[T], name Const[T], assignment Expression[T]) NameContext[T] {
+	out := Let(name, assignment, contextualized)
+	out.tailedContext = true
+	return out
+}
+
+// Setter method for struct member `contextualized`
+//
+// NOTE: panics if contextualized is nil
+func (cxt NameContext[T]) SetContextualized(contextualized Expression[T]) NameContext[T] {
+	if contextualized == nil {
+		panic("nil value error: argument passed for parameter `contextualized` cannot be nil")
+	}
+
+	cxt.contextualized = contextualized
+	return cxt
+}
+
+func (cxt NameContext[T]) GetName() Const[T] {
+	return cxt.name
+}
+
+func (cxt NameContext[T]) GetAssignment() Expression[T] {
+	return cxt.assignment
+}
+
+func (cxt NameContext[T]) GetContextualized() Expression[T] {
+	return cxt.contextualized
+}
+
 func assembleNameContextString(name, assignment, contextualized string, tailedContext bool) string {
 	if tailedContext {
 		return contextualized + " where " + name + " = " + assignment
@@ -154,7 +202,12 @@ func (nameCxt NameContext[T]) ForceRequest() Expression[T] {
 }
 
 func (nameCxt NameContext[T]) ExtractFreeVariables(dummyVar Variable[T]) []Variable[T] {
-	return append(nameCxt.ExtractFreeVariables(dummyVar), nameCxt.assignment.ExtractFreeVariables(dummyVar)...)
+	cxtzdVars := nameCxt.contextualized.ExtractFreeVariables(dummyVar)
+	assgnVars := nameCxt.assignment.ExtractFreeVariables(dummyVar)
+	if nameCxt.tailedContext {
+		return append(cxtzdVars, assgnVars...)
+	}
+	return append(assgnVars, cxtzdVars...)
 }
 
 func (nameCxt NameContext[T]) Collect() []T {
